cfa2f8db651d780fea203a8401af46b3b99433b1
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-06-20 23:15:28 +0200

    Initial Commit

---
+++ laungageChange/script.js
@@ -0,0 +1,20 @@
+var data = {
+    "english":
+    {
+        "title": "Hello Everyone!",
+        "description":
+        "Hello, everyone! This is the LONGEST TEXT EVER! I was inspired by the various other longest texts ever on the internet, and I wanted to make my own. So here it is! This is going to be a WORLD RECORD! This is actually my third attempt at doing this. The first time, I didn't save it. The second time, the Neocities editor crashed. Now I'm writing this in Notepad, then copying it into the Neocities editor instead of typing it directly in the Neocities editor to avoid crashing. It sucks that my past two attempts are gone now. Those actually got pretty long. Not the longest, but still pretty long. I hope this one won't get lost somehow. Anyways, let's talk about WAFFLES! I like waffles. Waffles are cool. Waffles is a funny word. There's a Teen Titans Go episode called Waffles where the word Waffles is said a hundred-something times. It's pretty annoying. There's also a Teen Titans Go episode about Pig Latin."
+    },
+    "chinese":
+    {
+        "title": "大家好!",
+        "description":
+        "大家好！ 这是有史以来最长的文字！ 我受到了互联网上各种其他最长文本的启发，我想制作自己的文本。 所以就在这里！ 这将成为世界纪录！ 这实际上是我第三次尝试这样做。 第一次，我没有保存它。 第二次，Neocities 编辑器崩溃了。 现在我在记事本中写这个，然后将它复制到 Neocities 编辑器中，而不是直接在 Neocities 编辑器中键入它以避免崩溃。 糟糕的是，我过去的两次尝试现在都没有了。 那些实际上很长。 不是最长的，但仍然很长。 我希望这个不会以某种方式迷路。 不管怎样，我们来谈谈华夫饼吧！ 我喜欢华夫饼。 华夫饼很酷。 华夫饼是一个有趣的词。 有一个名为 Waffles 的 Teen Titans Go 插曲，其中 Waffles 这个词被说了一百多次。 这很烦人。 还有一个关于 Pig Latin 的 Teen Titans Go 剧集。"
+    },
+    "spanish":
+    {
+        "title": "¡Hola a todos!",
+        "description":
+        "Hola a todos! ¡Este es el TEXTO MÁS LARGO DE LA HISTORIA! Me inspiré en los otros textos más largos de Internet y quería hacer el mío propio. ¡Asi que aqui esta! ¡Esto va a ser un RÉCORD MUNDIAL! Este es en realidad mi tercer intento de hacer esto. La primera vez, no lo guardé. La segunda vez, el editor de Neocities colapsó. Ahora escribo esto en el Bloc de notas y luego lo copio en el editor de Neocities en lugar de escribirlo directamente en el editor de Neocities para evitar que se cuelgue. Apesta que mis últimos dos intentos se hayan ido ahora. Esos en realidad se hicieron bastante largos. No es el más largo, pero sigue siendo bastante largo. Espero que este no se pierda de alguna manera. De todos modos, ¡hablemos de WAFFLES! Me gustan los waffles. Los gofres son geniales. Waffles es una palabra graciosa. Hay un episodio de Teen Titans Go llamado Waffles donde la palabra Waffles se dice cientos de veces. Es bastante molesto. También hay un episodio de Teen Titans Go sobre Pig Latin."
+    }
+}
